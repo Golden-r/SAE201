b372e8d1f583f83c955182650236ddb72e4d2859
Timothé Larbi <timothe.larbi@etu.univ-lehavre.fr>
2026-06-02 09:41:18 +0200

    modif Liste  Plateau.java

--- src/metier/Plateau.java
+++ src/metier/Plateau.java
@@ -10,7 +10,7 @@ public class Plateau
 
     private int nbSymbole ;
 
-    private ArrayList<Zone> ensZone ;
+    private Zone[] ensZone ;
 
     /* CONSTRUCTEUR */
     public Plateau ( int taille , int nbCouleur , int nbSymbole ) 
@@ -18,7 +18,7 @@ public class Plateau
         this.taille      = taille ;
         this.nbCouleur   = nbCouleur ;
         this.nbSymbole   = nbSymbole ;
-        this.ensZone     = new ArrayList<Zone>() ;
+        this.ensZone     = new Zone[taille * taille]  ;
     }
 
     /* GETTERS */
@@ -27,22 +27,11 @@ public class Plateau
     public int getNbSymbole () { return this.nbSymbole ; }
 
     /* SETTERS */
-    public void setTaille    ( int taille    ) { this.taille = taille       ; }
+    public void setTaille    ( int taille    ) { this.taille    = taille    ; }
     public void setNbCouleur ( int nbCouleur ) { this.nbCouleur = nbCouleur ; }
     public void setNbSymbole ( int nbSymbole ) { this.nbSymbole = nbSymbole ; }
 
     /* METHODES */
-    public boolean ajouterZone ( Zone zone )
-    {
-        this.ensZone.add( zone ) ;
-        return true ;
-    }
-    public boolean supprimerZone ( Zone zone )
-    {
-        if (zone == null) return false;
-        
-        this.ensZone.remove( zone ) ;
-        return true ;
-    }
+   
 
 }
