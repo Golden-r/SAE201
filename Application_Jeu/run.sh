d8d62dc9381b8c20e1d3efd176d56cdc6c39765d
Rayan Azaanoune <rayan.azaanoune@etu.univ-lehavre.fr>
2026-06-04 09:02:47 +0200

    Ajout de la deuxieme branche pour le Jeu.

--- Application_Jeu/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac -d  class @compile.list
-java  -cp class controleur.Controleur "$1"
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if (! [ -d "class" ]); then
+  mkdir class
+fi
+
 javac -d  class @compile.list
 java  -cp class controleur.Controleur "$1"
