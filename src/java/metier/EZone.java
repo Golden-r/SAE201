332ab1aac7f8cde32542308a4e382fce9183ecd8
Rayan Azaanoune <rayan.azaanoune@etu.univ-lehavre.fr>
2026-06-05 09:02:19 +0200

    Ajout de description de EZone + qlq ajustements

--- src/java/metier/EZone.java
+++ src/java/metier/EZone.java
@@ -2,7 +2,7 @@ package metier;
 
 /* SAE 2.01 | Développement d'une application 
 * EZone
-*
+*	Description : Enumération des différentes zones de la ville
 * Date     : 02/06/2026
 * @author  : AZAANOUNE Rayan , BASSAM YOUSSIF Youssif , FERRIER Mathys , LARBI Timothe 
 * Groupe   : 4
@@ -10,26 +10,26 @@ package metier;
 
 public enum EZone
 {
-	ZONE1  ("Le Pôle Industriel"),
-	ZONE2  ("Les Docks Marchands"),
-	ZONE3  ("Le Hub Logistique"),
-	ZONE4  ("Le Centre de Données"),
-	ZONE5  ("L'Écoquartier"),
-	ZONE6  ("Le Centre Historique"),
-	ZONE7  ("Les Coteaux Résidentiels"),
-	ZONE8  ("Le Quartier Ouvrier"),
-	ZONE9  ("Le Nouveau Lotissement"),
-	ZONE10 ("Le Centre d'Affaires"),
-	ZONE11 ("Les Halles Marchandes"),
-	ZONE12 ("Le Technopôle"),
-	ZONE13 ("La Promenade des Arts"),
-	ZONE14 ("Le Pôle Hospitalier"),
-	ZONE15 ("Le Campus Universitaire"),
-	ZONE16 ("Le Pôle Multimodal"),
+	ZONE1  ("Le Pôle Industriel"       ),
+	ZONE2  ("Les Docks Marchands"      ),
+	ZONE3  ("Le Hub Logistique"        ),
+	ZONE4  ("Le Centre de Données"     ),
+	ZONE5  ("L'Écoquartier"            ),
+	ZONE6  ("Le Centre Historique"     ),
+	ZONE7  ("Les Coteaux Résidentiels" ),
+	ZONE8  ("Le Quartier Ouvrier"      ),
+	ZONE9  ("Le Nouveau Lotissement"   ),
+	ZONE10 ("Le Centre d'Affaires"     ),
+	ZONE11 ("Les Halles Marchandes"    ),
+	ZONE12 ("Le Technopôle"            ),
+	ZONE13 ("La Promenade des Arts"    ),
+	ZONE14 ("Le Pôle Hospitalier"      ),
+	ZONE15 ("Le Campus Universitaire"  ),
+	ZONE16 ("Le Pôle Multimodal"       ),
 	ZONE17 ("Le Quartier Administratif"),
-	ZONE18 ("Le Complexe Énergétique"),
-	ZONE19 ("Le Val de l'Eau"),
-	ZONE20 ("Le Grand Parc Urbain");
+	ZONE18 ("Le Complexe Énergétique"  ),
+	ZONE19 ("Le Val de l'Eau"          ),
+	ZONE20 ("Le Grand Parc Urbain"     );
 
 	private String libelle;
 
