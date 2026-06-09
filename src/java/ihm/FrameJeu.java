288a5b32d0005b1c292f7936017b1c15c1c64e83
you8bssm <youssifputrus@gmail.com>
2026-06-09 16:46:33 +0200

    jeu debut , premier commit

--- src/java/ihm/FrameJeu.java
+++ src/java/ihm/FrameJeu.java
@@ -1,41 +1,33 @@
 package ihm;
 
-
 import controleur.Controleur;
 
-import metier.ESymbole;
-import metier.ECouleur;
-import metier.Plateau;
 
 import javax.swing.*;
 import java.awt.*;
 
 /* SAE 2.01 | Développement d'une application 
-* FrameCreation
+* FrameJeu
 *
 * Date     : 02/06/2026
 * @author  : AZAANOUNE Rayan , BASSAM YOUSSIF Youssif , FERRIER Mathys , LARBI Timothe 
 * Groupe   : 4
 */
 
-public class FrameCreation extends JFrame
+public class FrameJeu extends JFrame
 {
-
-    public FrameCreation( Controleur ctrl )
+	public FrameJeu( Controleur ctrl)
     {
-
-        this.setTitle("Application création - Groupe 4 - ");
+        
+        this.setTitle("Application jeu - Groupe 4");
 		this.setSize(1200, 120);
 		this.setLocation(50, 100);
 
 		
-		this.add( new PanelCreation( ctrl ) );
+		this.add(new PanelJeu (ctrl ));
 
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
     }
-
-    
-
 }
